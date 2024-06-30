2a5cee220f255d1cc71784169facaadba8da6c94
Nada Obaid <nada.s.obaidd@gmail.com>
2024-07-01 01:19:27 +0300

    some update

--- src/Modules/Products/Products.Router.js
+++ src/Modules/Products/Products.Router.js
@@ -5,8 +5,6 @@ import fileUpload, {fileValidation} from "../../Services/multer.js"
 const router = Router({mergeParams: true}); 
 
 router.get('/getProducts', ProductController.getProducts)
-router.get('/getBodyProducts', ProductController.getBodyProducts)
-router.get('/getFaceProducts', ProductController.getFaceProducts)
 router.post("/CreateProducts", fileUpload(fileValidation.image).single('image'), ProductController.CreateProducts);
 router.delete('/hardDelete/:id', ProductController.hardDelete)
 router.delete('/softDelete/:id', ProductController.softDelete)
